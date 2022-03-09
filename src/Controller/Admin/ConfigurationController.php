0d8f29bfeb84bdeaf0466b465d49c9b6690d9f31
flaviopellizzer <flappio.pelliccia@gmail.com>
2022-03-09 16:00:04 +0100

    Configuration :: first release ConfigurationController.php
    Reference
    https://devdocs.prestashop.com/1.7/modules/testing/ci-cd/

---
+++ src/Controller/Admin/ConfigurationController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Salus per Aquam
+ * Copyright since 2021 Flavio Pellizzer and Contributors
+ * <Flavio Pellizzer> Property
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * License
+ *
+ * @author    Flavio Pellizzer
+ * @copyright Since 2021 Flavio Pellizzer and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ */
+
+namespace Salusperaquam\Controller\Admin;
+
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+
+class ConfigurationController extends FrameworkBundleAdminController
+{
+    // stuff
+}
