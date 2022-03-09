7c08ee2e578f5a3844cb68a4368d40996630bade
flaviopellizzer <flappio.pelliccia@gmail.com>
2022-03-09 22:19:57 +0100

    License MIT

--- salusperaquam.php
+++ salusperaquam.php
@@ -6,20 +6,15 @@
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * This source file is subject to the MIT
  * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/AFL-3.0
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to https://devdocs.prestashop.com/ for more information.
- *
- * License
+ * https://opensource.org/licenses/MIT
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to flappio.pelliccia@gmail.com so we can send you a copy immediately.
  *
  * @author    Flavio Pellizzer
- * @copyright Since 2021 Flavio Pellizzer and Contributors
- * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ * @copyright Since 2021 Flavio Pellizzer
+ * @license   https://opensource.org/licenses/MIT
  */
