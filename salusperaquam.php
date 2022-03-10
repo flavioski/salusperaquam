599c15941e85d75fab451ae9ec4a7ce7be2d01ae
flaviopellizzer <flappio.pelliccia@gmail.com>
2022-03-10 22:43:46 +0100

    Salus Per Aquam :: __construct() method

--- salusperaquam.php
+++ salusperaquam.php
@@ -18,3 +18,48 @@
  * @copyright Since 2021 Flavio Pellizzer
  * @license   https://opensource.org/licenses/MIT
  */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class SalusPerAquam extends Module
+{
+    public function __construct($name = null, Context $context = null)
+    {
+        $this->name = 'salusperaquam';
+        $this->tab = 'administrator';
+        $this->version = '1.0.0';
+        $this->author = 'Flavio Pellizzer';
+        $this->need_instance = 0;
+
+        parent::__construct($name, $context);
+
+        $this->displayName = $this->getTranslator()->trans(
+            'Salus Per Aquam',
+            [],
+            'Modules.SalusPerAquam.Admin'
+        );
+
+        $this->description =
+            $this->getTranslator()->trans(
+                'Salus Per Aquam webService thermae.',
+                [],
+                'Modules.SalusPerAquam.Admin'
+            );
+
+        $this->ps_versions_compliancy = [
+            'min' => '1.7.6.0',
+            'max' => _PS_VERSION_,
+        ];
+    }
+
+    /**
+     * This function is required in order to make module compatible with new translation system.
+     *
+     * @return bool
+     */
+    public function isUsingNewTranslationSystem()
+    {
+        return true;
+    }
+}
