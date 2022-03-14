0803ea1e1afde8ef751df9f731656bd6cf6c3b13
flaviopellizzer <flappio.pelliccia@gmail.com>
2022-03-14 09:46:40 +0100

    Webpack

---
+++ js/treatments/index.js
@@ -0,0 +1,46 @@
+/**
+ * Salus per Aquam
+ * Copyright since 2021 Flavio Pellizzer and Contributors
+ * <Flavio Pellizzer> Property
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/MIT
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to flappio.pelliccia@gmail.com so we can send you a copy immediately.
+ *
+ * @author    Flavio Pellizzer
+ * @copyright Since 2021 Flavio Pellizzer
+ * @license   https://opensource.org/licenses/MIT
+ */
+
+import Grid from '@components/grid/grid';
+import ReloadListActionExtension from '@components/grid/extension/reload-list-extension';
+import ExportToSqlManagerExtension from '@components/grid/extension/export-to-sql-manager-extension';
+import FiltersResetExtension from '@components/grid/extension/filters-reset-extension';
+import SortingExtension from '@components/grid/extension/sorting-extension';
+import LinkRowActionExtension from '@components/grid/extension/link-row-action-extension';
+import SubmitGridExtension from '@components/grid/extension/submit-grid-action-extension';
+import SubmitBulkExtension from '@components/grid/extension/submit-bulk-action-extension';
+import BulkActionCheckboxExtension from '@components/grid/extension/bulk-action-checkbox-extension';
+import SubmitRowActionExtension from '@components/grid/extension/action/row/submit-row-action-extension';
+
+const $ = window.$;
+
+$(() => {
+  const treatmentsGrid = new Grid('treatment');
+
+  treatmentsGrid.addExtension(new ReloadListActionExtension());
+  treatmentsGrid.addExtension(new ExportToSqlManagerExtension());
+  treatmentsGrid.addExtension(new FiltersResetExtension());
+  treatmentsGrid.addExtension(new SortingExtension());
+  treatmentsGrid.addExtension(new LinkRowActionExtension());
+  treatmentsGrid.addExtension(new SubmitGridExtension());
+  treatmentsGrid.addExtension(new SubmitBulkExtension());
+  treatmentsGrid.addExtension(new BulkActionCheckboxExtension());
+  treatmentsGrid.addExtension(new SubmitRowActionExtension());
+});
---
+++ js/webpack.config.js
@@ -0,0 +1,31 @@
+/**
+ * Salus per Aquam
+ * Copyright since 2021 Flavio Pellizzer and Contributors
+ * <Flavio Pellizzer> Property
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/MIT
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to flappio.pelliccia@gmail.com so we can send you a copy immediately.
+ *
+ * @author    Flavio Pellizzer
+ * @copyright Since 2021 Flavio Pellizzer
+ * @license   https://opensource.org/licenses/MIT
+ */
+
+/**
+ * Three mode available:
+ *  build = production mode
+ *  build:analyze = production mode with bundler analyzer
+ *  dev = development mode
+ */
+module.exports = () => (
+  process.env.NODE_ENV === 'production' ?
+    require('./.webpack/prod.js')() :
+    require('./.webpack/dev.js')()
+);
