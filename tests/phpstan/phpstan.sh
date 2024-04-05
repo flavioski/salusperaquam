0067f57b975e4e7898fd378c4bb0def2d3b369af
flaviopellizzer <flappio.pelliccia@gmail.com>
2024-04-05 12:12:40 +0200

    CI/CD fix path phpstan.sh

--- tests/phpstan/phpstan.sh
+++ tests/phpstan/phpstan.sh
@@ -25,4 +25,4 @@ docker run --rm --volumes-from temp-ps \
        -e _PS_ROOT_DIR_=/var/www/html \
        --workdir=/var/www/html/modules/salusperaquam phpstan/phpstan:0.12.54 \
        analyse \
-       --configuration=/var/www/html/modules/salusperaquam/tests/php/phpstan/phpstan-$PS_VERSION.neon --error-format github
+       --configuration=/var/www/html/modules/salusperaquam/tests/phpstan/phpstan-$PS_VERSION.neon --error-format github
